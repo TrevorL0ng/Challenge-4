bd2d2b6ad45aa884de347daa740a1432ec5dedfd
Trevor L <trevorlong992@gmail.com>
2021-11-28 18:08:16 -0500

    Post dinner and nap update. Big updates coming soon

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Setting our DOM elements in order used on HTML
+var viewBigScore = document.getElementById("see-scores");
+var timerClock = document.getElementById("timer");
+var introCard = document.getElementById("intro-container");
+var startButton = document.getElementById("start");
+var questionCard = document.getElementById("question-container");
+var answerButtons = document.getElementById("answers");
+var correctAnswer = document.getElementById("right");
+var wrongAnswer = document.getElementById("wrong");
+
+
