40dabac8a0fa9586afe7e78534a74c9852cd8c0f
Trevor L <trevorlong992@gmail.com>
2021-11-28 18:27:18 -0500

    DOM elements added, now just for the logic. Hopefully wont be too bad

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@ var viewBigScore = document.getElementById("see-scores");
 var timerClock = document.getElementById("timer");
 var introCard = document.getElementById("intro-container");
 var startButton = document.getElementById("start");
-var questionCard = document.getElementById("question-container");
+var questionScreen = document.getElementById("question-container");
+var questionCard = document.getElementById("question");
 var answerButtons = document.getElementById("answers");
 var correctAnswer = document.getElementById("right");
 var wrongAnswer = document.getElementById("wrong");
-
+var outroCard = document.getElementById("outro-container");
+var userScore = document.getElementById("your-score");
+var userInitials = document.getElementById("score-form");
 
