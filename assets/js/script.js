bb90d31d8ec02c49a312daf029de028710962813
Trevor L <trevorlong992@gmail.com>
2021-11-28 19:09:22 -0500

    Hitting a mental wall. Need a break but not much time left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Setting our DOM elements in order used on HTML
 var viewBigScore = document.getElementById("see-scores");
 var timerClock = document.getElementById("timer");
-var introCard = document.getElementById("intro-container");
+var introScreen = document.getElementById("intro-container");
 var startButton = document.getElementById("start");
 var questionScreen = document.getElementById("question-container");
 var questionCard = document.getElementById("question");
@@ -11,4 +11,29 @@ var wrongAnswer = document.getElementById("wrong");
 var outroCard = document.getElementById("outro-container");
 var userScore = document.getElementById("your-score");
 var userInitials = document.getElementById("score-form");
+var highScoreScreen = document.getElementById("score-container");
+var bigScoresCard = document.getElementById("bigScores-list");
+var backButton = document.getElementById("go-back");
+var clearButton = document.getElementById("clear-bigScores");
+var timeLeft;
+var score = 0;
+var endQuiz;
 
+// Questions to be used in the quiz 
+
+var questions = [
+    {question:"What operator is used to increment by 1? in Javascript?",
+        answer:"3.) ++",
+        choices: [{choice: "1.) +1"}, {choice:"2.) 1up"}, {choice:"3.) ++"}, {choice:"4.) ^"}]
+    },
+    // {question:""
+    //     answer: 
+    //     choices: []
+    // },
+    // {question:
+    //     answer: 
+    //     choices: []
+    // },
+];
+
+console.log(questions);
